00e9c03793a1a40b52b139e8ba52911a13190717
soluchin <rafisolichin@gmail.com>
2023-07-20 22:17:28 +0700

    add some changes

--- src/main/java/com/rimba/technicaltest/Entity/Transaction.java
+++ src/main/java/com/rimba/technicaltest/Entity/Transaction.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
